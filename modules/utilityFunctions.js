1c092bf25ff4ae55eea500da0f4f30308e2c480a
Alfred Peter <alpeter91@gmail.com>
2023-06-12 19:53:11 +0300

    Fix bugs preventing script from running

--- modules/utilityFunctions.js
+++ modules/utilityFunctions.js
@@ -7,15 +7,23 @@ class UtilityFunctions {
   // Getting from Local Storage
   static getStorage = () => {
     const retrievedBooks = JSON.parse(localStorage.getItem('bookData'));
-    return retrievedBooks;
+    if (retrievedBooks && Array.isArray(retrievedBooks)) {
+      return retrievedBooks;
+    } else {
+      return []; // Return an empty array if storage is empty or not an array
+    }
   };
 
   // Showing books on browser  -> uses container
   static showBooks = (element, template) => {
     const retrievedBooks = this.getStorage();
-    element.innerHTML = retrievedBooks
-      .map((book, index) => template(book, index))
-      .join('');
+    if (retrievedBooks.length > 0) {
+      element.innerHTML = retrievedBooks
+        .map((book, index) => template(book, index))
+        .join('');
+    } else {
+      element.innerHTML = `<h2>There are no books in storage</h2>`;
+    }
   };
 
   static addBook(array, book) {
